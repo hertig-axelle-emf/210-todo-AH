d3fa54e90b72a390f31912d5b1107aac357f73db
Axelle Hertig <axelle.hertig@studentfr.ch>
2025-02-20 10:54:52 +0100

    test

--- src/backend/server.js
+++ src/backend/server.js
@@ -29,4 +29,4 @@ app.use((err, req, res, next) => {
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-//test workflow10 test 4
+//test workflow10 test main
